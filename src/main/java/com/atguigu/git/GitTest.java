93b608953999ed44888bee5a090cb2ca564367ab
liubo <288989@atguigu.com>
2022-10-07 19:26:04 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 刘波
+ * @version 1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
